64ccfd3ac698369d5c35f691ff6663095bb4723d
Mark Petrie <petrie.mark@gmail.com>
2017-09-05 13:49:16 -0700

    Add category and expense models, as well as passing unit tests

---
+++ test/unit/category.test.js
@@ -0,0 +1,27 @@
+
+const Category = require('../../lib/models/category');
+const { assert } = require('chai');
+const expectedValidation = () => {
+    throw new Error('expected validation errors');
+};
+
+describe('Category', () => {
+    it('validates good model', () => {
+        const category = new Category({
+            name: 'Office Supplies',
+            budget: 1000,
+            department: 'HR'
+        });
+        return category.validate();
+    });
+
+    it('fails validation with missing field', () => {
+        const category = new Category();
+        return category.validate()
+            .then(expectedValidation, err => {
+                const errors = err.errors;
+                assert.ok(errors.name && errors.name.kind === 'required');
+                assert.ok(errors.budget && errors.budget.kind === 'required');
+            });
+    });
+});
