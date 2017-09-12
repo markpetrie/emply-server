7cd3e3f0e0315664a62313fa4132ac594be1e784
Mark Petrie <petrie.mark@gmail.com>
2017-09-11 20:52:41 -0700

    Modifications to support adding expenses

--- test/unit/expense.test.js
+++ test/unit/expense.test.js
@@ -1,15 +1,29 @@
 
 const Expense = require('../../lib/models/expense');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const Category = require('../../lib/models/category');
+chai.use(chaiHttp);
 const { assert } = require('chai');
+const app = require('../../lib/app');
+const request = chai.request(app);
+
 const expectedValidation = () => {
     throw new Error('expected validation errors');
 };
 
 describe('Expense', () => {
+    const newCategory = {
+        name: 'Office Supplies',
+        amount: 1000,
+        department: 'HR'
+    };
+
     it('validates good model', () => {
         const expense = new Expense({
             name: 'Printer ink refills',
             amount: 47,
+            category: '59b08f3b23d915147077cf1f'
         });
         return expense.validate();
     });
